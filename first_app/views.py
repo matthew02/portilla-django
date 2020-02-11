88cbdd6f46ecd29afb5e1776a6fa240dc1246425
James Smith <matthew02@users.noreply.github.com>
2020-02-10 22:30:49 -0600

    Initial commit.

---
+++ first_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    my_dict = {
+        'insert_me': 'Hello, I am from views.py!',
+    }
+    return render(request, 'index.html', context=my_dict)
+    #return HttpResponse('Hello, World!')
