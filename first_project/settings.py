ff796ca7ac1a8093f9ef7771e89829651ca5fc25
James Smith <matthew02@users.noreply.github.com>
2020-02-12 23:29:53 -0600

    Updated INSTALLED_APPS to modern convention

--- first_project/settings.py
+++ first_project/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'first_app',
+    'first_app.apps.FirstAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
