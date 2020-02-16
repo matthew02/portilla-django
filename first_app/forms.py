746e41ddd20e104329e79aed2528dce09e75c1ec
James Smith <matthew02@users.noreply.github.com>
2020-02-16 11:36:49 -0600

    Added manual form validation

--- first_app/forms.py
+++ first_app/forms.py
@@ -1,7 +1,16 @@
 from django import forms
+from django.core import validators
 
 
 class FormName(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
+    botcatcher = forms.CharField(required = False, widget = forms.HiddenInput)
+    
+    def clean_botcatcher(self):
+        botcatcher = self.cleaned_data['botcatcher']
+        if len(botcatcher) > 0:
+            raise forms.ValidationError('Invalid response.')
+        return botcatcher
+    
