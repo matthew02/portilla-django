0edbaea9625e819833fbc9b1f336554b0b7df753
James Smith <matthew02@users.noreply.github.com>
2020-02-16 11:21:50 -0600

    Added basic form data processing

--- first_app/views.py
+++ first_app/views.py
@@ -9,9 +9,20 @@ from .forms import FormName
 
 def index(request):
     webpages = AccessRecord.objects.order_by('date')
-    dates = {'access_records': webpages}
-    return render(request, 'first_app/index.html', context=dates)
+    context = {'access_records': webpages}
+    return render(request, 'first_app/index.html', context=context)
 
 def form_view(request):
-    form = {'form': FormName()}
-    return render(request, 'first_app/form.html', context=form)
+    if request.method == 'POST':
+        form = FormName(request.POST)
+        
+        if form.is_valid():
+            print('Validation success.')
+            print(f'NAME: {form.cleaned_data["name"]}')
+            print(f'EMAIL: {form.cleaned_data["email"]}')
+            print(f'TEXT: {form.cleaned_data["text"]}')
+    else:
+        form = FormName()
+
+    context = {'form': form}
+    return render(request, 'first_app/form.html', context=context)
