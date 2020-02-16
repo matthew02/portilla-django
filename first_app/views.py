331d219e47513d59f7d68488a9fc0193c0985140
James Smith <matthew02@users.noreply.github.com>
2020-02-15 20:28:05 -0600

    Connected first_app models to templates

--- first_app/views.py
+++ first_app/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+from first_app.models import AccessRecord, Topic, Webpage
+
+
 def index(request):
-    my_dict = {
-        'insert_me': 'Hello, I am from views.py!',
-    }
-    return render(request, 'first_app/index.html', context=my_dict)
+    webpages = AccessRecord.objects.order_by('date')
+    dates = {'access_records': webpages}
+    return render(request, 'first_app/index.html', context=dates)
