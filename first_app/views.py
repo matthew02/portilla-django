c10df1f11faa88367e52db047709e1a4082ec496
James Smith <matthew02@users.noreply.github.com>
2020-02-15 23:06:26 -0600

    Added basic form (no POST processing)

--- first_app/views.py
+++ first_app/views.py
@@ -3,9 +3,15 @@ from django.http import HttpResponse
 
 
 from first_app.models import AccessRecord, Topic, Webpage
+#from . import forms
+from .forms import FormName
 
 
 def index(request):
     webpages = AccessRecord.objects.order_by('date')
     dates = {'access_records': webpages}
     return render(request, 'first_app/index.html', context=dates)
+
+def form_view(request):
+    form = {'form': FormName()}
+    return render(request, 'first_app/form.html', context=form)
